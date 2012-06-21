9eaeebad332cd19f0ea382c1dcc1ccca5d411159
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2012-06-21 06:22:06 +0530

    generate 55 random records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+def rand_string
+  (0...10).map{ ('a'..'z').to_a[rand(26)] }.join
+end
 
 RoundStat.delete_all
-RoundStat.create(:name => "Shep", :setinfo_pass => "abcd", :kills => 100, :deaths => 50, :hits => 1000, :shots => 100000, :headshots => 10)
-RoundStat.create(:name => "foo", :setinfo_pass => "efgh", :kills => -1, :deaths => 500, :hits => 1000, :shots => 100000, :headshots => 10)
+RoundStat.create(:name => "Shep", :setinfo_pass => "abcd", :kills => 1000, :deaths => 50, :hits => 1000, :shots => 100000, :headshots => 10)
+55.times { RoundStat.create(:name => rand_string, :setinfo_pass => rand_string, :kills => rand(1000), :deaths => 50+rand(1000), :hits => rand(10000), :shots => rand(10000), :headshots => rand(100)) }
