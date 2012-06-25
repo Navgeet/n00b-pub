e1517a2c874635bf24063c8999931427fa3f8f2c
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2012-06-26 02:32:28 +0530

    add tab bar and show all tables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @round_stats = RoundStat.all
+    @daily_stats = DailyStat.all
+    @perma_stats = PermaStat.all
     # render 'round_stats/index'
   end
 end
