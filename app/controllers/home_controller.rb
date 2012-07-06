b175dc89fceb4c8077718c807a755c2c0f68a4ff
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2012-07-06 20:36:07 +0530

    add beta-stage flash message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 
 class HomeController < ApplicationController
   def index
+    flash.alert = "Please note that the site is in beta stage. Help out by reporting bugs and suggesting enhancements. Also note that current accounts are temporary and may be lost."
     @users = User.all
     @ts = RoundStat.find_all_by_team(1)
     @cts = RoundStat.find_all_by_team(2)
