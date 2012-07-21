4345f09f5e517ba76fb4a08ddc64426e5083cde8
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2012-07-21 10:26:06 +0530

    add more flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 
 class HomeController < ApplicationController
   def index
-    flash.alert = "Please note that the site is in beta stage. Help out by reporting bugs and suggesting enhancements. Also note that current accounts are temporary and may be lost."
+    flash.alert = "Please note that the site is in beta stage. <a href=\"/bugs\">Help out</a> by reporting bugs and suggesting enhancements. Also note that current accounts are temporary and may be lost.".html_safe
+    flash.notice = "New here? Read this if you are <a href=\"/faq\">getting started</a>.".html_safe
+    flash[:tip] = "Tip: Login and check \"Remember Me\" to get rid of these messages.".html_safe
     @users = User.all
     @ts = RoundStat.find_all_by_team(1)
     @cts = RoundStat.find_all_by_team(2)
