9034e48cc6faf870dedbd62ff86566644b7727c5
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2012-08-05 06:42:41 +0530

    clear flash messages if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,16 @@
 
 class HomeController < ApplicationController
   def index
-    flash.alert = "Please note that the site is in beta stage. <a href=\"/bugs\">Help out</a> by reporting bugs and suggesting enhancements. Also note that current accounts are temporary and may be lost.".html_safe
-    flash.notice = "New here? Read this if you are <a href=\"/faq\">getting started</a>.".html_safe
-    flash[:tip] = "Tip: Login and check \"Remember Me\" to get rid of these messages.".html_safe
+    flash.now[:alert] = "Please note that the site is in beta stage. <a href=\"/bugs\">Help out</a> by reporting bugs and suggesting enhancements. Also note that current accounts are temporary and may be lost.".html_safe
+    flash.now[:notice] = "New here? Read this if you are <a href=\"/faq\">getting started</a>.".html_safe
+    flash.now[:tip] = "Tip: Login and check \"Remember Me\" to get rid of these messages.".html_safe
+
+    if user_signed_in?
+      flash.now[:alert] = nil
+      flash.now[:notice] = nil
+      flash.now[:tip] = nil
+    end
+
     @users = User.all
     @ts = RoundStat.find_all_by_team(1)
     @cts = RoundStat.find_all_by_team(2)
