e0333bb807d069fac981adf4902a35f381e5c870
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2012-08-07 07:30:02 +0530

    add map_frequency pie chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
-
 class HomeController < ApplicationController
   def index
+
+    require 'gchart'
+    map_hash = File.read("/home/nav/cs/cstrike/map_log").split.inject(Hash.new(0)) { |h,e| id, n = e.split('-'); h[id] += n.to_i; h } 
+    @map_names = Array.new 
+    @map_times = Array.new 
+
+    map_hash.each do |key, value| 
+      @map_names << key 
+      @map_times << value 
+    end 
+
+    @pie_chart = Gchart.pie(:title => 'Map frequency', :size => '600x420', :data => @map_times, :labels => @map_names )
+
     flash.now[:alert] = "Please note that the site is in beta stage. <a href=\"/bugs\">Help out</a> by reporting bugs and suggesting enhancements.".html_safe
     flash.now[:notice] = "New here? Read this if you are <a href=\"/faq\">getting started</a>.".html_safe
     flash.now[:tip] = "Tip: Login and check \"Remember Me\" to get rid of these messages.".html_safe
